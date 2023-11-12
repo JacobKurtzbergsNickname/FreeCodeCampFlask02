8d588ddabf9c2dbc660f0855e3e67e65ea07dc81
Kirby <korbinian.brunner@web.de>
2023-11-12 22:21:33 +0100

    Bootstrap installed

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/css_tutorial")
+def css_tutorial():
+    return render_template("css_tutorial.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
