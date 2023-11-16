9cf2fc90469cb22f845fcdcad8b0d6690b313ac3
Kirby <korbinian.brunner@web.de>
2023-11-16 21:52:05 +0100

    Dividing into components

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+    {
+        "id": 1,
+        "title": "Applied Biologist",
+        "location": "Tabanga, Isla Sombra",
+        "salary": "300000 gross per year"
+    },
+    {
+        "id": 2,
+        "title": "Encarceration Engineer",
+        "location": "Tabanga, Isla Sombra",
+        "salary": "400000 gross per year"
+    },
+    {
+        "id": 3,
+        "title": "Hands-on Biologist",
+        "location": "Tabanga, Isla Sueño",
+        "salary": "150000 gross per year"
+    },
+    {
+        "id": 4,
+        "title": "Traversal Application Developer",
+        "location": "Sowahige"
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", jobs=jobs)
 
 @app.route("/css_tutorial")
 def css_tutorial():
